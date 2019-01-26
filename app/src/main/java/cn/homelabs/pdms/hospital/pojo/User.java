ddfa39b03bf4841b571205139bfe4f144d9d7be0
liuhao <liuhao@homelabs.cn>
2019-01-26 11:46:50 +0800

    update code.

--- app/src/main/java/cn/homelabs/pdms/hospital/pojo/User.java
+++ app/src/main/java/cn/homelabs/pdms/hospital/pojo/User.java
@@ -7,6 +7,7 @@ public class User {
 
     @Inject
     public User(String name) {
+        this.name = name;
     }
 
     public String getName() {
---
+++ app/src/main/java/cn/homelabs/pdms/hospital/viewmodel/MainViewModel.java
@@ -0,0 +1,12 @@
+package cn.homelabs.pdms.hospital.viewmodel;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.support.annotation.NonNull;
+
+public class MainViewModel extends AndroidViewModel {
+
+    public MainViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
